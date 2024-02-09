d49dcf27ce69a0d9242379046ef979b36f24c55e
pradanna <pradanamahendra@gmail.com>
2024-02-09 18:39:16 +0700

    home

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -126,34 +126,53 @@
     <div class="g-info">
         <div class="info container ">
             <span class="score">5</span>
-            <span class="definition">Tahun Pengalaman</span>
+            <div class="d-flex flex-column ">
+                <span class="definition">Tahun </span>
+                <span class="definition">Pengalaman </span>
+            </div>
         </div>
         <div class="line"></div>
         <div class="info container ">
             <span class="score">2K+</span>
-            <span class="definition">Titik Iklan Strategis</span>
+            <div class="d-flex flex-column ">
+                <span class="definition">Titik Iklan </span>
+                <span class="definition">Strategis </span>
+            </div>
         </div>
         <div class="line"></div>
         <div class="info container ">
             <span class="score">8K+</span>
-            <span class="definition">Happy Customer</span>
+            <div class="d-flex flex-column ">
+                <span class="definition">Happy</span>
+                <span class="definition">Customer </span>
+            </div>
         </div>
         <div class="line"></div>
         <div class="info container ">
             <span class="score">4,6</span>
-            <span class="definition">Overal Rating</span>
+            <div class="d-flex flex-column ">
+                <span class="definition">Overal</span>
+                <span class="definition">Rating </span>
+            </div>
         </div>
     </div>
 
     <div class="g-container-left">
-        <p>Titik Strategis Tersebar di Seluruh Indonesia</p>
-        <p>Menyediakan lebih dari 10.000 titik iklan billboard. Sebaran titik iklan yang luas, mampu menjangkau hingga
-            seluruh kota di Indonesia. Kemudahan dalam sewa billboard berbeda wilayah. Atur harga sewa billboard Anda,
-            pilih durasi tayang mulai dari mingguan ataupun bulanan.
-        </p>
-        <a class="btn-warning">
-            Pasang Iklan Sekarang
-        </a>
+        <div class="content">
+            <p class="title">Titik Strategis Tersebar di Seluruh Indonesia</p>
+            <p class="text">Menyediakan lebih dari 10.000 titik iklan billboard. Sebaran titik iklan yang luas, mampu
+                menjangkau hingga
+                seluruh kota di Indonesia. Kemudahan dalam sewa billboard berbeda wilayah. Atur harga sewa billboard
+                Anda,
+                pilih durasi tayang mulai dari mingguan ataupun bulanan.
+            </p>
+            <div class="mb-5"></div>
+            <a class="btn-pasangiklan">
+                Pasang Iklan Sekarang
+            </a>
+        </div>
+
+        <img src="{{ asset('images/local/resize-indo.png') }}" alt="titik-iklan-billboard-indonesia" />
     </div>
     <script src="{{ asset('js/fitty.min.js') }}"></script>
     <script>
