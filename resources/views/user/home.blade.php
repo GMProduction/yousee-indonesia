85c11c07b0febf62c2e70b50bd796ad880642772
pradanna <pradanamahendra@gmail.com>
2024-04-01 14:14:28 +0700

    home

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -211,15 +211,13 @@
         });
 
         splide.mount();
-
-        <
-        script type = "text/javascript" >
-            window.onload = function() {
-                var wookmark1 = new Wookmark("#wookmark1", {
-                    outerOffset: 10, // Optional, the distance to the containers border
-                    itemWidth: 210, // Optional, the width of a grid item
-                });
-            }; <
-        />
+    </script>
+    <script type="text/javascript">
+        window.onload = function() {
+            var wookmark1 = new Wookmark("#wookmark1", {
+                outerOffset: 10, // Optional, the distance to the containers border
+                itemWidth: 210, // Optional, the width of a grid item
+            });
+        };
     </script>
 @endsection
